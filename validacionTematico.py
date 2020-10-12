5e04ccea67304c1acc239cc5185bfab2480d3811
Roberto Dominguez Vera <robertdominguezvera10@gmail.com>
2020-10-12 15:24:12 -0500

    Error de matriz de error compuesta

--- validacionTematico.py
+++ validacionTematico.py
@@ -414,7 +414,11 @@ class validacion(DialogType,DialogUi):
         unicos = np.unique(true)
         unicos2 = np.unique(pred)
         letras = ["a","b","c","d","e","f","g","h","i","j","k","l",
-                "m","n","o","p","q","r","s","t","v","w","x","y","z"]
+                "m","n","o","p","q","r","s","t","v","w","x","y","z",
+		"aa","bb","cc","dd","ee","ff","gg","hh","ii","jj","kk","ll",
+                "mm","nn","oo","pp","qq","rr","ss","tt","vv","ww","xx","yy","zz",
+		"aaa","bbb","ccc","ddd","eee","fff","ggg","hhh","iii","jjj","kkk","lll",
+                "mmm","nnn","ooo","ppp","qqq","rrr","sss","ttt","vvv","www","xxx","yyy","zzz"]
         encode = []
         encode2 = []
         j = 0
